4c08ec40d4e242bfe58496bbea74d5e7753b76eb
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-06 23:45:29 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+
+const newHeader=document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+const yourname='Tuğba'
+newHeader.innerHTML=`${yourname} is the champion`
